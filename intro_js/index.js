5be00a5e76623b1ccf821437c4fea411d212406e
Eduardo Marzagao <eduardo.marzagao@hotmail.com>
2026-03-14 08:37:31 -0300

    Ex calculadora

--- intro_js/index.js
+++ intro_js/index.js
@@ -18,29 +18,44 @@ console.log(`Aprendendo ${linguagem}`)
 // res.f1() // Roda com o cont final
 // res.f2()
 
-let concessionaria = {
-    nome: "Concessionaria",
-    CNPJ: "1234",
-    endereco: {
-        logradouro: 'Rua B',
-        numero: 50,
-        bairro: {
-            nome: "aaaa",
-        }
-    },
-    estoque: [  //lista
-        {
-            modelo: "ka",
-            marca: "ford",
-            placa: "aad-1234"
-        },
-        {
-            modelo: "fusca",
-            marca: "vw",
-            placa: "add-4455"
-        }
-    ]
-}
+// let concessionaria = {
+//     nome: "Concessionaria",
+//     CNPJ: "1234",
+//     endereco: {
+//         logradouro: 'Rua B',
+//         numero: 50,
+//         bairro: {
+//             nome: "aaaa",
+//         }
+//     },
+//     estoque: [  //lista
+//         {
+//             modelo: "ka",
+//             marca: "ford",
+//             placa: "aad-1234"
+//         },
+//         {
+//             modelo: "fusca",
+//             marca: "vw",
+//             placa: "add-4455"
+//         }
+//     ]
+// }
 
-console.log(concessionaria.estoque[0].modelo)
-console.log(concessionaria["estoque"][1]["modelo"])
+// console.log(concessionaria.estoque[0].modelo)
+// console.log(concessionaria["estoque"][1]["modelo"])
+
+// const calculadora = {
+//     operacoes: {
+//         soma: (a, b) => {return a + b},
+//         subtracao: (a, b) => a - b,
+//         multiplicacao: function (a, b){
+//             return a * b
+//         },
+//         divisao: (a, b) => a / b
+//     }
+// }
+
+// console.log(calculadora.operacoes.soma(2,3))
+// console.log(calculadora["operacoes"]["subtracao"](4, 5))
+// console.log(calculadora.operacoes.divisao(1,3))
