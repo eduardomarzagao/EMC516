75e082fa9bdc221b764e01219a3afa0637d8f088
Eduardo Marzagao <eduardo.marzagao@hotmail.com>
2026-03-21 08:35:05 -0300

    Add: Then/Catch

--- intro_js/index.js
+++ intro_js/index.js
@@ -60,19 +60,19 @@ console.log(`Aprendendo ${linguagem}`)
 // console.log(calculadora["operacoes"]["subtracao"](4, 5))
 // console.log(calculadora.operacoes.divisao(1,3))
 
-function demorada(tempo){
-    const dataAtualMais2Segundos = new Date().getTime() + tempo
-    while(new Date().getTime() <= dataAtualMaisTempo);
-    const d = 8 + 2 * 6
-    return d
-}
+// function demorada(tempo){
+//     const dataAtualMais2Segundos = new Date().getTime() + tempo
+//     while(new Date().getTime() <= dataAtualMaisTempo);
+//     const d = 8 + 2 * 6
+//     return d
+// }
 
-setTimeout(() => {
-    console.log("Agendada pelo setTimeout")
-}, 0)
-const dataAtualMais5Segundos = new Date().getTime() + 5000
-while(new Date().getTime() <= dataAtualMais5Segundos);
-console.log("Terminando o script principal...")
+// setTimeout(() => {
+//     console.log("Agendada pelo setTimeout")
+// }, 0)
+// const dataAtualMais5Segundos = new Date().getTime() + 5000
+// while(new Date().getTime() <= dataAtualMais5Segundos);
+// console.log("Terminando o script principal...")
 
 // const a = 2 + 3
 // const b = 6 * 1
@@ -91,3 +91,55 @@ console.log("Terminando o script principal...")
 // console.log("Eu primeiro...")
 // console.log("Agora eu...")
 // console.log("Sempre serei a ultima...")
+
+// const fs = require('fs')
+// const nomeArquivo = 'arquivo.txt'
+
+// const exibirConteudo = (erro, conteudo) => {
+//     console.log("A")
+//     if(erro) {
+//         console.log(`Deu erro> ${erro}`)
+//     }
+//     else{
+//         console.log(`Conteúdo: ${conteudo}`)
+//         const dobro = Number(conteudo.toString()) * 2
+//         // Mais uma função callback
+//         const finalizar = (erro) => {
+//             console.log(erro ? "Deu erro ao escrever o dobro" : "Ok, escreveu o dobro")
+//             console.log('C')
+//         }
+//         fs.writeFile('dobro.txt', dobro.toString(), finalizar)
+//         console.log('D')
+//     }
+// }
+// fs.readFile(nomeArquivo, exibirConteudo)
+// console.log("B")
+
+// const calculoDemorado = (n) => {
+//     let ac = 0
+//     for(let i = 1; i <= n; i++)
+//         ac += i
+//     return ac
+// }
+const calculoDemorado = (n) => {
+    return new Promise((resolve, reject) =>{
+        // Se n for negativo, propagar um erro
+        // Caso contrário, continua como já era
+        if(n > 0){
+            let ac = 0
+            for(let i = 1; i <= n; i++)
+                ac += i
+            resolve(ac)
+        }
+        else{
+            reject(`Erro, n menor que 0! Valor de n:${n}`)
+        }
+        
+    })
+}
+const minhaPromise = calculoDemorado(1000)
+//then/catch
+minhaPromise
+.then((resultado) => {console.log(`Resultado: ${resultado}`)})
+.catch((erro) => {console.log(`Erro: ${erro}`)})
+console.log("A")
