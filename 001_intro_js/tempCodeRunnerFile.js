44a308e741514b88bab5db6e4aafe4649f86a9a9
Eduardo Marzagao <eduardo.marzagao@hotmail.com>
2026-05-16 10:39:50 -0300

    atualizei até o onde o professor estava

--- 001_intro_js/tempCodeRunnerFile.js
+++ 001_intro_js/tempCodeRunnerFile.js
@@ -1,1 +1,12 @@
-u
+const eAgora = () =>{
+    let cont = 1
+    const f1 = () => console.log(cont)
+    cont++
+    const f2 = () => console.log(cont)
+    cont++
+    return {f1, f2}
+}
+const res = eAgora()
+// As linhas a seguir executam as funções f1 e f2, que estão dentro de res
+res.f1() // Roda com o cont final
+res.f2()
---
+++ 003_microsservicos/observacoes/index.js
@@ -0,0 +1,40 @@
+const axios = require('axios')
+const express = require('express')
+const { v4: uuidV4 } = require('uuid')
+const app = express()
+app.use(express.json())
+
+const observacoesPorLembreteId = {}
+
+//:id é um placeholder
+//exemplo: /lembretes/12/observacoes
+app.get('/lembretes/:id/observacoes', (req, res) => {
+    res.send(observacoesPorLembreteId[req.params.id] || [])
+})
+
+app.post('/lembretes/ :id/observacoes', async (req, res) => {
+    const idObs = uuidV4()
+    const { texto } = req.body
+    const id = req.params.id
+
+    const observacoesDoLembrete = observacoesPorLembreteId[id] || []
+    observacoesDoLembrete.push({ id: idObs, texto})
+    observacoesPorLembreteId[id] = observacoesDoLembrete
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: 'ObservacaoCriada',
+        dados: {
+            id: idObs, texto: texto, lembreteId: id
+        }
+    })
+    res.status(201).send(observacoesDoLembrete)
+})
+
+app.post('/eventos', (req, res) => {
+    const evento = req.body
+    console.log(evento)
+    res.end()
+})
+
+app.listen(5001, (() => {
+    console.log('Observacoes. Porta 5001.')
+}))
